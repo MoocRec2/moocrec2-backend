1ea56ef71aed64334f9248dba26a38915352f801
Sameer Mohamed <smrbasil4@gmail.com>
2019-08-14 19:49:41 +0530

    Created Basic Login Endpoint

--- engagement-mapper/routes/users.js
+++ engagement-mapper/routes/users.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// Login Endpoint
+router.post('/login', (req, res) => {
+  if (req.body.username === undefined || req.body.username === null ||
+    req.body.password === undefined || req.body.password === null) {
+    res.status(400).send({ message: 'Bad Request' })
+    return
+  }
+
+  // Perform authentication
+  if (req.body.username === 'admin' && req.body.password === 'admin') {
+    res.status(200).send({ message: 'Logged in Successfully' })
+    return
+  }
+
+  res.status(403).send({ message: 'Incorrect Credentials' })
+
+})
+
 module.exports = router;
