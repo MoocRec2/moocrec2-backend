e422ed09e10c4a73a094c4b7fe6bf71d4245ced8
Sameer3079 <smrbasil4@gmail.com>
2019-08-23 15:05:23 +0530

    Created Search Endpoint

--- engagement-mapper/routes/courses.js
+++ engagement-mapper/routes/courses.js
@@ -9,7 +9,18 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/top-rated', (req, res) => {
-    courseModel.find({ score: { $exists: 1 } }).sort({score: -1}).limit(10).exec().then(documents => {
+    courseModel.find({ score: { $exists: 1 } }).sort({ score: -1 }).limit(10).exec().then(documents => {
+        res.status(200).send(documents)
+    }, error => {
+        console.error(error)
+        res.status(500).send('Error')
+    })
+})
+
+router.get('/search/:query', (req, res) => {
+    var query = req.params.query
+    console.log(query)
+    courseModel.find({ title: { $regex: new RegExp(query), $options: 'i' } }).limit(10).exec().then(documents => {
         res.status(200).send(documents)
     }, error => {
         console.error(error)
